754a06a442b8306d7eed5cad3e2a7be0bb9d4f6b
дмитрий козлов <hance@yandex.ru>
2024-02-17 00:15:23 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,23 @@
+﻿public class FizzBuzz
+{
+    public static void Main(string[] args)
+    {
+        FizzBuzzProgram(Console.WriteLine, 35);
+    }
+    public static void FizzBuzzProgram(Action<string?> outputMethod, int n) =>
+        outputMethod(Enumerable.Range(1, n)
+            .Select(FizzBuzzPipeline)
+            .Aggregate((a, b) => a + "\r\n" + b));
+
+    static readonly List<Func<int, string?>> handlers =
+    [
+        (n) => n % 3 == 0 ? "Fizz" : null,
+        (n) => n % 5 == 0 ? "Buzz" : null,
+        (n) => n % 7 == 0 ? "Qux" : null
+    ];
+    static string? FizzBuzzPipeline(int i)=>
+        handlers.Select(handler => handler(i))
+                .Where(r => r != null)
+                .DefaultIfEmpty(i.ToString())
+                .Aggregate((a, b) => a + b);
+}
