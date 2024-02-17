d995c091f797a0c1bda1ed08b8a04bdfa8c5bc2e
дмитрий козлов <hance@yandex.ru>
2024-02-18 00:23:19 +0300

    красивое

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -5,36 +5,14 @@
         FizzBuzzProgram(Console.WriteLine, 350);
     }
     public static void FizzBuzzProgram(Action<string?> outputMethod, int n) =>
-        outputMethod(Enumerable.Range(1, n)
-        .Select(FizzBuzzPipeline)
-        .Aggregate((a, b) => a + "\r\n" + b));
+        outputMethod(String.Join("\r\n", 
+            Enumerable.Range(1, n).Select(FizzBuzzPipeline)));
 
-    static int[] primes (int N) => 
-        Enumerable.Range(2, N)
-        .Where(IsPrime)
-        .ToArray();
-
-    public static bool IsPrime(int number) =>
-        number >= 2 
-        && Enumerable.Range(2, (int)Math.Sqrt(number) - 1)
-           .All(i => number % i != 0);
-
-    static Func<int, string?>[] handlers(int N) => primes(N).Select(prime =>
-        new Func<int, string?>(n => n % prime == 0 ? prime.ToString() + "**" : null)
-    ).ToArray();
-
-    public static string FizzBuzzPipeline(int i)
-    {
-        var result = String.Join("", handlers(i).Select(f => f(i)));
-        return result != "" ? result : i.ToString();
-    }
-    static object[][] mapper = [[3, "Три"], [5, "Пять"], [7, "Семь"]];
-    public static string Do(int value) =>
-        string.Join("", mapper
-            .Where(item => value % (int)item[0] == 0)
-            .Select(item => (string)item[1]));
-
-
+    static readonly IEnumerable<Func<int, string?>> handlers = [(n) => n % 3 == 0 ? "Три" : null, (n) => n % 5 == 0 ? "Пять" : null];
+    
+    public static string FizzBuzzPipeline(int i) => Counter(i, String.Join("", handlers.Select(f => f(i))));
+    
+    private static string Counter(int i, string result) => result != "" ? result : i.ToString();
 
     static void FizzBuzzProgram2(Action<string?> outputMethod, int n)
     {
