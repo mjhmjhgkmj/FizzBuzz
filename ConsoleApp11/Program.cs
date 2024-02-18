74dbcbb84cb2fce5eb629c1ef2286a0844d5edef
дмитрий козлов <hance@yandex.ru>
2024-02-19 00:08:54 +0300

    parallel

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -2,17 +2,29 @@
 {
     public static void Main(string[] args)
     {
-        FizzBuzzProgram(Console.WriteLine, 350);
+        Console.WriteLine(FizzBuzzProgram(350));
     }
-    public static void FizzBuzzProgram(Action<string?> outputMethod, int n) =>
-        outputMethod(String.Join("\r\n", 
-            Enumerable.Range(1, n).Select(FizzBuzzPipeline)));
+    public static string FizzBuzzProgram(int n) =>
+        String.Join("\r\n", 
+            Enumerable.Range(1, n).Select(FizzBuzzPipelineM));
 
-    static readonly IEnumerable<Func<int, string?>> handlers = [(n) => n % 3 == 0 ? "Три" : null, (n) => n % 5 == 0 ? "Пять" : null];
+    static readonly Func<int, string?>[] handlers = [(n) => n % 3 == 0 ? "Три" : null, (n) => n % 5 == 0 ? "Пять" : null];
     
     public static string FizzBuzzPipeline(int i) => Counter(i, String.Join("", handlers.Select(f => f(i))));
-    
+
+    public static string FizzBuzzPipelineM(int i)
+    {
+        var results = handlers.AsParallel().Select(handler => handler(i)).Where(r => r != null);
+        return results.Any() ? string.Concat(results) : i.ToString();
+    }
+
     private static string Counter(int i, string result) => result != "" ? result : i.ToString();
+
+     static string Updater(int n)
+    {
+        string result = $"{(n % 3 == 0 ? "Fizz" : "")}{(n % 5 == 0 ? "Buzz" : "")}{(n % 7 == 0 ? "Qux" : "")}";
+        return String.IsNullOrEmpty(result) ? n.ToString() : result;
+    }
 
     static void FizzBuzzProgram2(Action<string?> outputMethod, int n)
     {
